409469a15635e54d124439e04921fd841a6832a2
michaelenoma <enomamichael@gmail.com>
2022-12-04 23:53:08 +0100

    Generate Image fragment

--- app/src/main/java/com/odogwudev/buildscriptstemplate/ai_inspire/presentation/ui/generate_image/GenerateImageFragment.kt
+++ app/src/main/java/com/odogwudev/buildscriptstemplate/ai_inspire/presentation/ui/generate_image/GenerateImageFragment.kt
@@ -1,10 +1,11 @@
-package com.odogwudev.buildscriptstemplate.ai_inspire
+package com.odogwudev.buildscriptstemplate.ai_inspire.presentation.ui.generate_image
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.odogwudev.buildscriptstemplate.ai_inspire.R
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,7 +17,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [generate_image.newInstance] factory method to
  * create an instance of this fragment.
  */
-class generate_image : Fragment() {
+class GenerateImageFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
