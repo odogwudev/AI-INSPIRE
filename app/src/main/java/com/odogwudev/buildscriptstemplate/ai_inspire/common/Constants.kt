a52398c8e63fe5f7c718272a5bfaf661b31a66d9
michaelenoma <enomamichael@gmail.com>
2023-01-06 23:53:25 +0100

    Helper common classes

---
+++ app/src/main/java/com/odogwudev/buildscriptstemplate/ai_inspire/common/Constants.kt
@@ -0,0 +1,12 @@
+package com.odogwudev.buildscriptstemplate.ai_inspire.common
+
+object Constants {
+    const val BASE_URL = "https://api.openai.com/v1/"
+    const val TOKEN = "Your Token Here"
+    const val GENERATE_IMAGE = "images/generations"
+    const val SIZE_256 = "256x256"
+    const val SIZE_512 = "512x512"
+    const val SIZE_1024 = "1024x1024"
+    const val MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1
+
+}
