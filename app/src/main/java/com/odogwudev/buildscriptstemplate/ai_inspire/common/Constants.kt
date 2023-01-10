f9aba18edf063e19e16fa5b128e28514d45f9af4
michaelenoma <enomamichael@gmail.com>
2023-01-11 00:39:14 +0100

    fix api key

--- app/src/main/java/com/odogwudev/buildscriptstemplate/ai_inspire/common/Constants.kt
+++ app/src/main/java/com/odogwudev/buildscriptstemplate/ai_inspire/common/Constants.kt
@@ -1,12 +1,15 @@
 package com.odogwudev.buildscriptstemplate.ai_inspire.common
 
+import com.odogwudev.buildscriptstemplate.ai_inspire.BuildConfig
+
 object Constants {
     const val BASE_URL = "https://api.openai.com/v1/"
-    const val TOKEN = "Your Token Here"
+    const val TOKEN = BuildConfig.API_KEY
     const val GENERATE_IMAGE = "images/generations"
     const val SIZE_256 = "256x256"
     const val SIZE_512 = "512x512"
     const val SIZE_1024 = "1024x1024"
     const val MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1
 
+    var myApiKey: String = BuildConfig.API_KEY
 }
